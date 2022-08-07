7730f95ce2317dac227e5d4058d1faed352ed7ca
Artem <remenusei@yandex.ru>
2022-08-08 02:14:37 +0700

    task3 and task4

---
+++ Homework/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+void FA (int [] arr){
+    for (int i=0;i<arr.Length;i++){
+        arr[i]=new Random().Next(200);
+    }
+}
+
+int FIND_SUM(int [] arr1){
+    int sum=0;
+    for (int i=1;i<arr1.Length;i+=2){
+        sum=sum+arr1[i];   
+    } 
+    return sum;   
+}
+int [] mas={-4,-6,89,6};
+int [] mas1=new int [10];
+FA(mas1);
+Console.WriteLine("Исходный массив "+ String.Join(", ",mas1));
+Console.WriteLine($"Сумма элементов расположенных на нечётных позициях равна - {FIND_SUM(mas1)}");
---
+++ Homework/Task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: Задайте с клавиатуры массив вещественных (double) чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// Задать размер (int) массива с клавиатуры
+// [3 7 22 2 78] -> 76
+
+void diff(double [] arr){
+    double max=arr[0];
+    double min=arr[0];
+    double dif=0;
+    for (int i=1;i<arr.Length;i++){
+        if (arr[i]>max)
+            max=arr[i];
+        if (arr[i]<min)
+            min=arr[i];
+    }
+    dif=max-min;
+    //Console.WriteLine(max);
+    //Console.WriteLine(min);
+    Console.WriteLine(dif);
+}
+
+Console.Write("Введитите длину масива- ");
+int n= Convert.ToInt32(Console.ReadLine());
+double [] mas=new double[n];
+for (int i=0;i<mas.Length;i++){
+
+    Console.Write($"Заполните элемент №{i} - ");
+    mas[i]=Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine("Исходный массив- "+String.Join(",", mas));
+diff(mas);
